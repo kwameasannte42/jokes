4d1cacc875cc2883fdd1d07240c4dc52f38b3f53
kwameasannte42 <coolllyod@gmail.com>
2023-04-08 02:28:20 -0400

    style

--- src/App.js
+++ src/App.js
@@ -6,6 +6,16 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   const [joke, setJoke] = useState("");
 
+  const [type, setType] =useState("")
+
+  const jokeType = () => {
+    Axios.get("https://official-joke-api.appspot.com/random_joke").then(
+      (res) => {
+        setType(res.data.type);
+      }
+    );
+  };
+
   const getJoke = () => {
     Axios.get("https://official-joke-api.appspot.com/random_joke").then(
       (res) => {
@@ -14,20 +24,34 @@ function App() {
     );
   };
 
+ 
+
   function handleClear() {
+    setType(null);
     setJoke(null);
+    
+  }
+  
+  function jokeAndType() {
+    getJoke()
+    jokeType()
   }
 
   return (
     <div className="whole">
-      <button onClick={getJoke}>Get Joke</button>
+      <button className="btn btn-danger" onClick={jokeAndType}>Get Joke</button>
       <div className="m-3">
-        <p>{joke}</p>
+        <div className="card">
+          <div className="card-body">
+            <h5 className="card-title">{type}</h5>
+            <p className="card-text">{joke}</p>
+          </div>
+        </div>
       </div>
       <div>
-        <button disabled={!joke}
-        className="button"
-        type="submit" onClick={handleClear}>Clear</button>
+        <button className="btn btn-outline-danger" disabled={!joke} type="submit" onClick={handleClear}>
+          Clear
+        </button>
       </div>
     </div>
   );
